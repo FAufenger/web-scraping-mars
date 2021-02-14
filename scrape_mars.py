0dba344b08457194da4d949913a8c6955c3141c0
Fox IV <73198491+FAufenger@users.noreply.github.com>
2021-02-13 21:56:02 -0500

    update scrape.py

--- scrape_mars.py
+++ scrape_mars.py
@@ -1,23 +1,159 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Import Dependencies
 from splinter import Browser
 from bs4 import BeautifulSoup
-
+import pandas as pd
+import requests
+import time
 
 def init_browser():
-    executable_path = {"executable_path": "chromedriver.exe"}
-    return Browser("chrome", **executable_path, headless=False)
+    executable_path = {'executable_path': 'chromedriver.exe'}
+    return Browser('chrome', **executable_path, headless=True)
 
 def scrape():
+    # Start browser
     browser = init_browser()
-    listings = {}
+    # Dictionary to holdall desired scraped variables
+    mars_news = {}
 
-    url = "html mars"
-    browser.visit(url)
+    ################ NASA Mars News #####################
 
-    html = browser.html
-    soup = BeautifulSoup(html, "html.parser")
+    # Scrape the NASA Mars News Site and collect the latest News Title and Paragraph Text. 
+    try:
+        # Open desired site in browser with splinter
+        url = 'https://mars.nasa.gov/news/'
+        browser.visit(url)
+        # Added time to allow page to fully load
+        time.sleep(3)
 
-    listings["headline"] = soup.find("a", class_="result-title").get_text()
-    listings["price"] = soup.find("span", class_="result-price").get_text()
-    listings["hood"] = soup.find("span", class_="result-hood").get_text()
+        # HTML Object
+        html = browser.html
+        # Parse HTML with Beautiful Soup
+        soup = BeautifulSoup(html, 'html.parser')
 
-    return listings
+        # Retrieve the latest element that contains news title and news_paragraph
+        results = soup.find('li', class_="slide")
+
+        # Extract first title and paragraph, and assign to variables
+        news_title = results.find('div', class_='content_title').text
+        news_p = results.find('div', class_='article_teaser_body').text
+        news_date = results.find('div', class_='list_date').text
+
+        # Append image to collection dictionary
+        mars_news['news_date'] = news_date
+        mars_news['news_title'] = news_title
+        mars_news['news_abtsract'] = news_p
+
+    except:
+        mars_news['news_date'] = 'news_date'
+        mars_news['news_title'] = 'news_title'
+        mars_news['news_abstract'] = 'news_p'
+
+
+    ############## JPL Mars Space Images - Featured Image ##############
+
+    # Visit the url for JPL Featured Space Image.
+    #Use splinter to navigate the site and find the image url for the current Featured Mars Image 
+    try:
+        image_url = 'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html#'
+        browser.visit(image_url)
+        # HTML Object
+        html = browser.html
+        # Parse HTML with Beautiful Soup
+        soup = BeautifulSoup(html, 'html.parser')
+        # Locate image with splinter
+        image_tag = soup.find('img', class_='headerimage')
+        image_location = image_tag.get('src')
+        # Combine base url with uri
+        featured_image_url = f'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/{image_location}'
+        # Append image to collection dictionary
+        mars_news['featured_image_url'] = featured_image_url
+    except:
+        mars_news['featured_image_url'] = 'featured_image_url'
+
+    ################### Mars Facts ################################
+
+    try:
+        # Use Pandas to scrape data
+        mars_facts_url = 'https://space-facts.com/mars/'
+        # Take first table for Mars facts
+        mars_facts_df = pd.read_html(mars_facts_url)[0]
+        # Column names
+        mars_facts_df.columns=["Description", "Mars"]
+        # Table for viewing
+        mars_facts_df.set_index("Description", inplace=True)
+        # Convert table to html
+        mars_table_html = mars_facts_df.to_html(index=True, header=True, border=0, justify="left")
+        mars_table_html = mars_table_html.replace("\n","")
+        # Append to collection dictionary
+        mars_news["mars_table_html"] = mars_table_html
+
+    except:
+        mars_news["mars_table_html"] = 'mars_table_htm'
+
+
+    ##################### Mars Hemispheres ##########################
+    
+    try:
+        # Visit Mars hemispheres website
+        usgs_astrogeology_mars_url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+        #Use splinter to navigate the site and find the image url for the current Featured Mars Image 
+        browser.visit(usgs_astrogeology_mars_url)
+        # HTML Object
+        landing_html = browser.html
+        # Parse HTML with Beautiful Soup
+        soup = BeautifulSoup(landing_html, 'html.parser')
+
+        # Set base url to add to extension later
+        hemisphere_base_url = 'https://astrogeology.usgs.gov/'
+        #Look on developer tool and find where each hemisphere is stored
+        hemisphere_locate = soup.find_all('div', class_='item')
+        # Create a list to store our dictionaries
+        hemisphere_image_urls = []
+        # Add a count to shorten overall run time
+        count = 0
+
+        # Create for  loop to pull out each hemisphere title and http address
+        for title in hemisphere_locate:
+            
+            # Store title
+            title = title.find('h3').text
+            # Click on title (link) to find full resolution image
+            browser.links.find_by_partial_text(title).click()
+            # Use time to delay after click
+            time.sleep(1)
+            
+            # Locate image partial url
+            image_html = browser.html
+            # Parse HTML with Beautiful Soup
+            soup = BeautifulSoup(image_html, 'html.parser')
+            # Looked on click page to fnd where full res image is stored
+            image_partial_url = soup.find('img', class_='wide-image')['src']
+            # combine partial and pre url
+            img_url = hemisphere_base_url + image_partial_url
+            # Append list with title and full size url to hemisphere_image_urls using a Python dictionary
+            hemisphere_image_urls.append({"title" : title, "img_url" : img_url})
+            
+            # Add count
+            count += 1
+            
+            # If not last search navigate back before starting loop over
+            if len(hemisphere_locate) > count:
+                browser.back()
+                time.sleep(1)
+            # Help save time by cutting last back and sleep
+            else:
+                break
+                
+        # Print list of dictionaries
+        mars_news["hemisphere_image_urls"] =hemisphere_image_urls
+
+    except:
+        mars_news["hemisphere_image_urls"] ='hemisphere_image_urls' 
+
+# Quit Browser
+browser.quit()
+
+
